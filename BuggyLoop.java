8854ab833e6edb4e5cff8ef2e22654d21ec740cf
BugHunter-X <askzenai@gmail.com>
2025-01-03 07:18:04 +0100

    Add BuggyLoop.java file

---
+++ BuggyLoop.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 5) {
+            System.out.println(x);
+            x++;
+            if (x == 3) {
+                continue;
+            }
+        }
+    }
+}
